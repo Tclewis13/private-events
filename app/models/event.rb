8bf4cfe303dac4084ead9d19eebfba2a5b1051ae
Tclewis13 <the3rdi@gmail.com>
2024-11-09 18:51:25 -0500

    project requirements done TODO: prevent duplicate attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendees, foreign_key: :event_id
   has_many :attendees, through: :event_attendees, source: :attendee
+
+  scope :past_events, -> { where("date < ?", Time.current) }
+  scope :future_events, -> { where("date >= ?", Time.current) }
 end
