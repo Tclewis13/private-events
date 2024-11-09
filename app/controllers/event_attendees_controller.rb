8e199fb482c61a0851e5eabc6ef1faec8b213073
Tclewis13 <the3rdi@gmail.com>
2024-11-08 22:25:52 -0500

    core functionality done TODO: dealing with duplicate attendee records TODO: separating past and future TODO: odin finishing touches

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,7 +1,5 @@
 class EventAttendeesController < ApplicationController
   def attend
-    puts params[:event_id]
-    puts current_user.id
     @event_attendee = EventAttendee.new(event_id: params[:event_id], attendee_id: current_user.id)
     if @event_attendee.save
       redirect_to Event.find(params[:event_id]), notice: "You have signed up to attend this event."
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :event_attendees, foreign_key: :event_id
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
-  has_many :attended_events, through: :event_attendees, source: :attendee
+  has_many :attended_events, through: :event_attendees, source: :event
 end
