41ba70574432a5f2ed55338160b7fcdb6fee5167
Tclewis13 <the3rdi@gmail.com>
2024-11-08 22:05:13 -0500

    event attending works, but is barebones. Need to fill out showing of attendees

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -3,10 +3,8 @@ class EventAttendeesController < ApplicationController
     puts params[:event_id]
     puts current_user.id
     @event_attendee = EventAttendee.new(event_id: params[:event_id], attendee_id: current_user.id)
-    @event_attendee.creator = Event.find(@event_attendee.event_id).creator.id
     if @event_attendee.save
-      format.html { redirect_to @event, notice: "You have signed up to attend this event." }
-      format.json { render :show, status: :created, location: @event }
+      redirect_to Event.find(params[:event_id]), notice: "You have signed up to attend this event."
     else
       puts @event_attendee.errors.full_messages
     end
