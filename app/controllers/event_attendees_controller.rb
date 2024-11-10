a7018e72659529d287c041a3bf715d9f84d5b03d
Tclewis13 <the3rdi@gmail.com>
2024-11-09 19:01:22 -0500

    duplicates solved. Project base done

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,10 +1,13 @@
 class EventAttendeesController < ApplicationController
   def attend
     @event_attendee = EventAttendee.new(event_id: params[:event_id], attendee_id: current_user.id)
-    if @event_attendee.save
+    @duplicate = EventAttendee.find_by(event_id: @event_attendee.event_id, attendee_id: @event_attendee.attendee_id)
+    if !@duplicate.nil?
+      redirect_to Event.find(params[:event_id]), alert: "You are already signed up to attend this event."
+    elsif @event_attendee.save
       redirect_to Event.find(params[:event_id]), notice: "You have signed up to attend this event."
     else
-      puts @event_attendee.errors.full_messages
+      redirect_to Event.find(params[:event_id]), alert: "Sign-up failed because #{@event_attendee.errors.full_messages}"
     end
   end
 end
